b62c39feb86e4265ece88b88ee4d83b459d0c4f8
Anneke Sinnema <mail@annekesinnema.nl>
2019-11-19 00:09:14 +0100

    ⚒ Add acceptance environment class if on staging

--- grav/user/themes/annekesinnema/js/main.js
+++ grav/user/themes/annekesinnema/js/main.js
@@ -1,5 +1,7 @@
 if (window.location.toString().includes("localhost")) {
 	document.getElementById('html').className = "dev-env";
+} else if (window.location.toString().includes("works")) {
+	document.getElementById('html').className = "acc-env";
 } else {
 	document.getElementById('html').className = "prod-env";
 }
