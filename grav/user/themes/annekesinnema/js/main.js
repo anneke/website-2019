62b2a864d0aa6c296234e10ac84041aa18dcc384
Anneke Sinnema <mail@annekesinnema.nl>
2019-11-19 22:59:16 +0100

    🕵️‍♀️ grid issues with IE11

--- grav/user/themes/annekesinnema/js/main.js
+++ grav/user/themes/annekesinnema/js/main.js
@@ -1,13 +1,14 @@
-if (window.location.toString().includes("localhost")) {
-	document.getElementById('html').className = "dev-env";
-} else if (window.location.toString().includes("works")) {
-	document.getElementById('html').className = "acc-env";
-} else {
-	document.getElementById('html').className = "prod-env";
-}
+// if (window.location.toString().includes("localhost")) {
+// 	document.getElementById('html').className = "dev-env";
+// } else if (window.location.toString().includes("works")) {
+// 	document.getElementById('html').className = "acc-env";
+// } else {
+// 	document.getElementById('html').className = "prod-env";
+// }
 
 const onButton = document.querySelector('.navigation__ontoggle');
 const offButton = document.querySelector('.navigation__offtoggle');
+const navContainer = document.querySelector('.site-navigation');
 const menu = document.querySelector('.mobile__navigation');
 const firstItem = document.querySelector('.navigation__list li:first-of-type a');
 
@@ -25,6 +26,7 @@ function toggleMenu() {
 	menu.classList.add('is-active');
 	onButton.setAttribute('hidden', true);
 	onButton.style.display = 'none'; 
+	navContainer.style.display = 'block'; 
 	firstItem.focus();
 	
 	for(var i=0; i < pageClickables.length; i++){
@@ -39,6 +41,7 @@ function toggleMenuOff() {
 	menu.classList.remove('is-active');
 	onButton.setAttribute('hidden', false);
 	onButton.style.display = 'flex';
+	navContainer.style.display = 'flex'; 
 
 	for(var i=0; i < pageClickables.length; i++){
 		pageClickables[i].removeAttribute('tabindex');
