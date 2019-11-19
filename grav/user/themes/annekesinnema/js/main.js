4f175b97062d7a3c08ca98773b2af90c9b425889
Anneke Sinnema <mail@annekesinnema.nl>
2019-11-19 01:40:04 +0100

    ⚒ Replaced css-toggled navigation with JavaScript

--- grav/user/themes/annekesinnema/js/main.js
+++ grav/user/themes/annekesinnema/js/main.js
@@ -5,3 +5,46 @@ if (window.location.toString().includes("localhost")) {
 } else {
 	document.getElementById('html').className = "prod-env";
 }
+
+const onButton = document.querySelector('.navigation__ontoggle');
+const offButton = document.querySelector('.navigation__offtoggle');
+const menu = document.querySelector('.mobile__navigation');
+const firstItem = document.querySelector('.navigation__list li:first-of-type a');
+
+const pageClickables = document.querySelectorAll(
+	'a, input:not([disabled]), ' +
+	'select:not([disabled]), textarea:not([disabled]),  ' +
+	'button:not([disabled]), *[tabindex] '
+);
+
+const menuClickables = menu.querySelectorAll(
+	'a, button:not([disabled]), *[tabindex] '
+);
+
+function toggleMenu() {
+	menu.classList.add('is-active');
+	onButton.setAttribute('hidden', true);
+	onButton.style.display = 'none'; 
+	firstItem.focus();
+	
+	for(var i=0; i < pageClickables.length; i++){
+		pageClickables[i].setAttribute('tabindex', -1);
+	}
+	for(var i=0; i < menuClickables.length; i++){
+		menuClickables[i].setAttribute('tabindex', 0);
+	}
+}; 
+
+function toggleMenuOff() {
+	menu.classList.remove('is-active');
+	onButton.setAttribute('hidden', false);
+	onButton.style.display = 'flex';
+
+	for(var i=0; i < pageClickables.length; i++){
+		pageClickables[i].removeAttribute('tabindex');
+	}
+}; 
+
+onButton.addEventListener('click', toggleMenu);
+offButton.addEventListener('click', toggleMenuOff);
+ 
